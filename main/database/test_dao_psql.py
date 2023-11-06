e42371a75696d095627b8ab38dc32200f56c07b3
HLeiTR <sy.hung@mail.utoronto.ca>
2023-11-05 19:41:23 -0500

    feat: Update GH Actions and testing script

--- main/database/test_dao_psql.py
+++ main/database/test_dao_psql.py
@@ -64,9 +64,10 @@ class TestDAOPostgreSQL:
             self.dao.add_user('t0001', 'test_to_slack_id', 'Scott')
             self.dao.add_corp_values('t0001', ['value1'])
             result = self.dao.add_message('t0001', 'test_channel_id',
-                                          'test_msg_id', time,
+                                          'test_channel_name', 'test_msg_id', time,
                                           'test_from_slack_id',
-                                          'test_to_slack_id', 'test_text',
+                                          'test_to_slack_id', 'from_username',
+                                          'to_username', 'test_text',
                                           ['value1'])
             assert result is True
         except Exception as e:
@@ -93,10 +94,28 @@ class TestDAOPostgreSQL:
             self.dao.create_workspace('t0001')
             values = self.dao.get_corp_values('t0001')
             assert isinstance(values, list)
-            assert len(values) == 0
+
+            # Default length should be 11 because
+            """
+            default_values = [
+                "Good Teamwork",
+                "Customer First",
+                "Innovation",
+                "Leadership",
+                "Continuous Learning",
+                "Problem Solving",
+                "Integrity",
+                "Collaboration",
+                "Passion",
+                "Flexibility",
+                "Accountability"
+            ]
+            """
+            assert len(values) == 11
+
             self.dao.add_corp_values('t0001', ['value1', 'value2'])
             values = self.dao.get_corp_values('t0001')
-            assert len(values) == 2
+            assert len(values) == 13
         except Exception as e:
             pytest.fail(f"Unexpected error: {e}")
 
